de733045d748a87231bdf47df8a90cc67df312d3
ywauto83 <ywauto@gmai.com>
2019-09-16 07:26:55 +1000

    add ssh key

--- delete-node-group-script.sh
+++ delete-node-group-script.sh
@@ -1,5 +1,5 @@
 # delete nodegroup
-eksctl delete nodegroup --cluster=my-eks-cluster-02  --name=ng-1-workersnode-private
+eksctl delete nodegroup --cluster=my-eks-cluster-02  --name=ng-1-workersnode-private --region ap-southeast-2
 
 # drain nodegroup
 eksctl drain nodegroup  --cluster=my-eks-cluster-02  --name=ng-1-workersnode-private
